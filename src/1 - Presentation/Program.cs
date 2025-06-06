186d28bdc51e987f69ce31b90e60acbee4e2816d
pedroabelardo <abelardosilv2014@gmail.com>
2025-06-06 03:08:08 -0300

    Adicionando configuração de mapeamento.

--- src/1 - Presentation/Program.cs
+++ src/1 - Presentation/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 MappingConfig.RegistrarMapeamentos();
 TypeAdapterConfig.GlobalSettings.Compile(); // para performance
 
